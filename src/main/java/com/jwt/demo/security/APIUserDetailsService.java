c116940c3edc504d5553510959af35d54087e557
unknown <gusdudco6@naver.com>
2024-05-24 13:15:34 +0900

    api user Details Service

--- src/main/java/com/jwt/demo/security/APIUserDetailsService.java
+++ src/main/java/com/jwt/demo/security/APIUserDetailsService.java
@@ -1,10 +1,18 @@
 package com.jwt.demo.security;
 
+import com.jwt.demo.domain.APIUser;
+import com.jwt.demo.dto.APIUserDTO;
 import com.jwt.demo.repository.APIUserRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 @Log4j2
@@ -12,4 +20,20 @@ import org.springframework.stereotype.Service;
 public class APIUserDetailsService implements UserDetailsService {
 
     private final APIUserRepository apiUserRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+
+        Optional<APIUser> result = apiUserRepository.findById(username);
+
+        APIUser apiUser = result.orElseThrow(() -> new UsernameNotFoundException("Cannot find user " + username));
+
+        APIUserDTO dto = new APIUserDTO(
+                apiUser.getMid(),
+                apiUser.getMpw(),
+                List.of(new SimpleGrantedAuthority("ROLE_USER"))
+        );
+
+        return dto;
+    }
 }
